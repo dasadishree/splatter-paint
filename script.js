20e84eb8f6e7b54ef732414f1829aaeebd3aa960
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2024-08-29 18:09:28 +0000

    miniscrap2

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+tool.fixedDistance = Math.random() * (45)+5;
+
 function onMouseMove(event) {
   var path = new Path.Circle({
     center: event.middlePoint,
-    radius: 10
+    radius: Math.round(Math.random() * 25) + 5
+  })
+  var path2 = new Path.Rectangle({
+    width:50,
+    height:50,
+    center: event.middlePoint
   })
 
   path.fillColor = {
-    hue: 0,
+    hue: event.count * 3,
     saturation: 1,
+    brightness: 1
+  }
+  path2.fillColor = {
+    hue: event.count * 3,
+    saturation:1,
     brightness: 1
   }
 }
