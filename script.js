e9c7b0aec3bc7799ea10229717e78679f1991980
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2024-08-29 22:22:28 +0000

    session1 complete

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 tool.fixedDistance = Math.random() * (45)+5;
+var synth = new Tone.Synth().toMaster()
+
 
 function onMouseMove(event) {
+  synth.triggerRelease()
+  synth.triggerAttack(Math.floor(Math.random() * 5000) + 200)
   var path = new Path.Circle({
     center: event.middlePoint,
     radius: Math.round(Math.random() * 25) + 5
